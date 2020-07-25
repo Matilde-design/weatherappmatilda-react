c8b3c1234519885987a1e1e445ba906429587bce
Matilde-design <matildeperdigaomaia@gmail.com>
2020-07-25 16:15:31 +0100

    Changes on the API

--- src/Description.js
+++ src/Description.js
@@ -3,39 +3,44 @@ import "./description.css";
 import axios from "axios";
 
 export default function Description(props) {
-
-    let apiKey = "6da49f4c9efbefcf042ac4b59c666478";
+    const apiKey = "6da49f4c9efbefcf042ac4b59c666478";
     let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${
       props.city
     }&appid=${apiKey}&units=metric`;
 
     axios.get(apiUrl).then(displayTemperature);
 
-    const [temperature, setTemperature] = useState(null);
-    const [description, setDescription] = useState(null);
-
+  
+    const [data, setData] = useState({loaded: false});
 
     function displayTemperature(response){
-setTemperature (Math.round(response.data.main.temp))
-setDescription (response.data.weather[0].description)
+
 console.log (response.data);
+
+setData({
+  loaded: true,
+  temperature: Math.round(response.data.main.temp),
+  description: response.data.weather[0].description,
+  wind: response.data.wind.speed,
+  humidity: response.data.main.humidity,
+
+})
     }
 
-    if (temperature) {return (
+    if (data.loaded) {return (
     <div className="daily-weather-info">
        <div className="row">
           <h1>{props.city} </h1>
        </div>
        <div className="row">
-          <p className="text-capitalize">{description}</p>
+          <p className="text-capitalize">{data.description}</p>
        </div>
        <div className="row hours"> <p> <small> Sunday 21, 11:00 </small></p> 
-       
        </div>
        <div className="row">
          <div className="col-6"> <span className="weather-image">☀︎</span> </div>
          <div className="col-6 temperatureinfo">
-          <p> <span className="temperature"> {temperature}  </span> 
+          <p> <span className="temperature"> {data.temperature}  </span> 
         <span className="units">
         <span  id="celsius" className="active">
           ºC{" "}
@@ -46,10 +51,16 @@ console.log (response.data);
         </span>
         </span>
       </p>
+      <ul>
+        <li>
+          Wind: {Math.round(data.wind)} km/h
+        </li>
+        <li>Humidity: {data.humidity}%</li>
+      </ul>
         </div>
       </div>
     
      </div>
   );}
-  else {return <h2>LOADING...</h2>}
+  else {return <h2>Loading...</h2>}
 }
