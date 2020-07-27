2d397ed08448c72246f6431d8e79db0a47ebc3c5
Matilde-design <matildeperdigaomaia@gmail.com>
2020-07-27 10:19:57 +0100

    Fixed issue with DateComponent

--- src/DateComponent.js
+++ src/DateComponent.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function DateComponent (props){
     let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
-    let day = days [props.date.getDay()];
+    let day = days[props.date.getDay()];
     let date = props.date.getDate();
     let hours = props.date.getHours();
     let minutes = props.date.getMinutes();
